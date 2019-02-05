18f9d370e50730827481e68fa86c58ca7855f47e
Christopher Reece <mrkita.reece@gmail.com>
2019-02-05 18:00:05 +0900

    square is constrained to circle

--- src/components/RadialControl/index.js
+++ src/components/RadialControl/index.js
@@ -3,26 +3,79 @@ import Interact from 'interactjs'
 import './index.styl'
 
 class RadialControl extends React.Component {
-  onMove(event) {
-   const target = event.target,
-    // keep the dragged position in the data-x/data-y attributes
-    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+  constructor(props) {
+    super(props)
+    this.state = {
+      controlPoint: {
+        x: 100,
+        y: 100
+      }
+    }
 
+  }
+
+  constraintPointToRadius(origin, destination) {
+    /* First get vector from destination and origin
+     * Then get unit vector
+     * then add radious to unit vector
+     *
+     */
+    const destRadius = -10
+
+    const vector = {
+      x: destination.x - origin.x,
+      y: destination.y - origin.y
+    }
+    const magnitude = Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2))
+    const unitVector = {
+      x: vector.x / magnitude,
+      y: vector.y / magnitude
+    }
+
+    //const newDestination = {
+      //x: (destination.x + destRadius) * unitVector.x,
+      //y: (destination.y + destRadius) * unitVector.y
+    //}
+    const newDestination = {
+      x: 100 * unitVector.x,
+      y: 100 * unitVector.y
+    }
+
+    return newDestination
+  }
+
+
+  onMove(event) {
+    const target = event.target
+    const controlPoint = this.state.controlPoint
+    const originPoint = {
+      x: 100,
+      y: 100
+    }
+
+    const destinationPoint = {
+      x: controlPoint.x + event.dx,
+      y: controlPoint.y + event.dy
+    }
+
+    const adjustedPoint = this.constraintPointToRadius(
+      originPoint,
+      destinationPoint
+    )
     // translate the element
     target.style.webkitTransform =
     target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+      'translate(' + adjustedPoint.x + 'px, ' + adjustedPoint.y + 'px)';
 
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
+    this.setState({
+      controlPoint: destinationPoint
+    })
   }
 
   componentDidMount() {
     Interact(this.square)
       .draggable({
-        onmove: this.onMove,
+        onmove: this.onMove.bind(this),
       })
   }
 
