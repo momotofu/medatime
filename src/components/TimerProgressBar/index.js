1238a7319d5ea419a8d8f0dc9c81a1b173bac359
Micah <micah@asuh.com>
2019-03-23 16:07:41 -0700

    Progress restart transition and styling
    1. Updated transition for restarting the timer so that it doesn't use a transition when resetting the timer
    2. Chrome and Firefox have a slight difference in element heights, so that the progress element in Chrome is slightly larger than Firefox. Had to adjust the height of the polyfill so that it fills Chrome and is only slightly larger in Firefox

--- src/components/TimerProgressBar/index.js
+++ src/components/TimerProgressBar/index.js
@@ -2,14 +2,15 @@ import React from 'react'
 import './index.styl'
 
 function TimerProgressBar(props) {
-	const { remainingSeconds, totalSeconds } = props
+	console.log(props)
+	const { remainingSeconds, totalSeconds, refill } = props
 	const percent = remainingSeconds * 100 / totalSeconds
-	const percentPolyfill = percent / 2 + '%'
+	const percentPolyfill = percent / 2 + .15 + '%'
 
 	return (
 		<div className="progressBarWrapper">
 			<progress className="TimerProgressBar" max={totalSeconds} value={remainingSeconds}>{percent}%</progress>
-			<span className="progressBarPolyfill" style={{width:percentPolyfill}}></span>
+			<span className={percent === 100 ? 'progressBarPolyfill progressBarRefill' : 'progressBarPolyfill'} style={{width:percentPolyfill}}></span>
 		</div>
 	)
 }
