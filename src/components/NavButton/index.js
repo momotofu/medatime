ad23103bb9547801c6bb54661b09274d604018dc
Micah <micah@asuh.com>
2019-04-22 15:13:08 -0700

    Nav Button position transition
    Added CSS transition to NavButton to move from left to right when going into and out of timer state

--- src/components/NavButton/index.js
+++ src/components/NavButton/index.js
@@ -11,12 +11,12 @@ function NavButton(props) {
 	const { isDisabled, isTimerVisible } = props
 	return(
 		<button
-			className="navButton"
+			className={isTimerVisible ? 'navButton is-timer' : 'navButton'}
 			onClick={handleClick}
 			disabled={isDisabled}
 		>
 			<span className="visuallyhidden">Switch Step</span>
-			{isTimerVisible ? <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><title>play</title><path fill="currentColor" d="M6 4l20 12L6 28z"/></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><title>play</title><path fill="currentColor" d="M6 4l20 12L6 28z"/></svg>}
+			<svg className="navButtonIcon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><title>play</title><path fill="currentColor" d="M6 4l20 12L6 28z"/></svg>
 		</button>
 	)
 }
