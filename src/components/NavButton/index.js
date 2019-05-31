2201bd92045af6aa67dcb9f77edd4dfa2e2d95df
Micah <micah@asuh.com>
2019-05-30 22:15:08 -0700

    Refined NavButton animation transition
    Using translateX was good for transition animation, but wasn't moving the button over the whole way. Better solution was with wrapper div containing another empty div to animate the flex property from 0 to 1, pushing the button to the right when the timer is active

--- src/components/NavButton/index.js
+++ src/components/NavButton/index.js
@@ -10,23 +10,22 @@ function NavButton(props) {
 
 	const { isDisabled, isLeft } = props
 	return (
-		<button
-			className={isLeft ? 'NavButton is-timer' : 'NavButton'}
-			onClick={handleClick}
-			disabled={isDisabled}
-		>
-			<span className="visuallyhidden">Switch Step</span>
-			<svg
-				className="navButtonIcon"
-				xmlns="http://www.w3.org/2000/svg"
-				width="32"
-				height="32"
-				viewBox="0 0 32 32"
-			>
-				<title>{isLeft ? 'Start Timer' : 'Go Back'}</title>
-				<path fill="currentColor" d="M6 4l20 12L6 28z" />
-			</svg>
-		</button>
+		<div className="NavButtonContainer">
+			<div className={isLeft ? 'NavButtonState is-timer' : 'NavButtonState'} />
+			<button className="NavButton" onClick={handleClick} disabled={isDisabled}>
+				<span className="visuallyhidden">Switch Step</span>
+				<svg
+					className="navButtonIcon"
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 32 32"
+				>
+					<title>{isLeft ? 'Start Timer' : 'Go Back'}</title>
+					<path fill="currentColor" d="M6 4l20 12L6 28z" />
+				</svg>
+			</button>
+		</div>
 	)
 }
 
